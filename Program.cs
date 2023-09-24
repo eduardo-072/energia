31ea651c7a6daf30a2d55624c9ef8349ca623501
eduardo-072 <eduardosilvaaa072@gmail.com>
2023-09-24 14:55:57 -0300

    foi feita a tarefa do dia 24

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double ConsumoEnergia, HorasDeUso;
+decimal custo, calculomês;
+
+Console.WriteLine("----- CONSUMO DE ENERGIA-----------\n");
+
+Console.Write("Consumo do aparelho em (kwh/mês):");
+ConsumoEnergia = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Horas de uso por dia:");
+HorasDeUso = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Custo de energia em $:");
+custo = Convert.ToDecimal(Console.ReadLine());
+
+calculomês = Convert.ToDecimal(ConsumoEnergia * HorasDeUso) * custo;
+
+Console.WriteLine($"\nSeu custo será de: {calculomês:C}");
